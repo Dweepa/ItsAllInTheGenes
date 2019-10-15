25476c17708672fa07e338d32ec01fcaeb75a4e7
KavyaVarma <kavya.varma98@gmail.com>
2019-10-15 21:16:04 +0530

    bug fix in test script

--- SNN/test_script.sh
+++ SNN/test_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 depths=(1 2 3 4)
-k=(1 2 3 4)
+k_val=(1 2 3 4)
 embedding_lengths=(8 16 32)
 epochs=(50 75 100)
 
@@ -14,10 +14,11 @@ for i in $(seq 0 $d_len)
 do
 	for j in $(seq 0 $emb_len)
 	do
-		python3 SNN.py ${depths[$i]} ${k[$i]} ${embedding_lengths[$j]}
+		echo python3 SNN.py ${depths[$i]} ${k_val[$i]} ${embedding_lengths[$j]}
 
 		for k in $(seq 0 $epoch_len)
 		do
+			echo x
 			python save_embeddings.py ${depths[$i]} ${k[$i]} ${embedding_lengths[$j]} ${epochs[$k]}
 			python internal_evaluation.py EMB_snn_${depths[$i]}_${k[$i]}_${embedding_lengths[$j]}-${epochs[$k]}
 		done
