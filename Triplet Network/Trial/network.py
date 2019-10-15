d8e8033eaa6dca628504e325dc4c21f2990aaf33
Dweepa <dweepa.prasad@gmail.com>
2019-10-15 12:20:48 +0530

    Restructured Files

---
+++ Triplet Network/Trial/network.py
@@ -0,0 +1,97 @@
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, Dropout, Input, LeakyReLU, concatenate
+from keras.layers.noise import AlphaDropout
+from keras.layers import Layer
+from tensorflow.python.keras import backend as K
+from sklearn.preprocessing import normalize
+
+
+# Creates 1 branch of the triplet network
+def create_base_network(n_dense=6,
+                        dense_units=16,
+                        activation='selu',
+                        dropout=AlphaDropout,
+                        dropout_rate=0.1,
+                        kernel_initializer='lecun_normal',
+                        optimizer='adam',
+                        num_classes=1,
+                        max_words=978):
+    if (activation == "leaky"):
+        model = Sequential()
+        model.add(Dense(dense_units, input_shape=(max_words,),
+                        kernel_initializer=kernel_initializer))
+        model.add(LeakyReLU(alpha=0.3))
+        model.add(dropout(dropout_rate))
+
+        for i in range(n_dense - 1):
+            model.add(Dense(dense_units, kernel_initializer=kernel_initializer))
+            model.add(LeakyReLU(alpha=0.3))
+            model.add(dropout(dropout_rate))
+        return model
+
+    model = Sequential()
+    model.add(Dense(dense_units, input_shape=(max_words,),
+                    kernel_initializer=kernel_initializer))
+    model.add(Activation(activation))
+    model.add(dropout(dropout_rate))
+
+    for i in range(n_dense - 1):
+        model.add(Dense(dense_units, kernel_initializer=kernel_initializer))
+        model.add(Activation(activation))
+        model.add(dropout(dropout_rate))
+    return model
+
+
+# triplet loss function
+def triplet_loss(y_true, y_pred, alpha=0.6):
+    """
+    Implementation of the triplet loss function
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor data
+            positive -- the encodings for the positive data (similar to anchor)
+            negative -- the encodings for the negative data (different from anchor)
+    Returns:
+    loss -- real number, value of the loss
+    """
+    print('y_pred.shape = ', y_pred)
+
+    total_length = y_pred.shape.as_list()[-1]
+    # print('total_length=',  total_length)
+    #     total_length =12
+
+    anchor = y_pred[:, 0:int(total_length * 1 / 3)]
+    positive = y_pred[:, int(total_length * 1 / 3):int(total_length * 2 / 3)]
+    negative = y_pred[:, int(total_length * 2 / 3):int(total_length * 3 / 3)]
+
+    # distance between the anchor and the positive
+    pos_dist = K.abs(K.sum(K.square(anchor - positive), axis=1))
+    print(pos_dist)
+    # distance between the anchor and the negative
+    neg_dist = K.abs(K.sum(K.square(anchor - negative), axis=1))
+    print(neg_dist)
+    # compute loss
+    basic_loss = pos_dist - (neg_dist + alpha)
+    loss = K.maximum(basic_loss, 0.0)
+
+    return loss
+
+
+# Creating full fledged triplet network with 3 inputs
+def create_siamese_network(shared_model, input_dim=978):
+    anchor_input = Input((input_dim,), name='anchor_input')
+    positive_input = Input((input_dim,), name='positive_input')
+    negative_input = Input((input_dim,), name='negative_input')
+
+    encoded_anchor = shared_model(anchor_input)
+    encoded_positive = shared_model(positive_input)
+    encoded_negative = shared_model(negative_input)
+
+    merged_vector = concatenate([encoded_anchor, encoded_positive, encoded_negative], axis=-1, name='merged_layer')
+
+    model = Model(inputs=[anchor_input, positive_input, negative_input], outputs=merged_vector)
+    model.compile(loss=triplet_loss, optimizer='adam')
+
+    return model
---
+++ Triplet Network/Working/crossvalidate.py
@@ -0,0 +1,82 @@
+# Import necessary libraries
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, Dropout, Input
+from keras.layers.noise import AlphaDropout
+from keras.layers import Layer
+from tensorflow.python.keras import backend as K
+from sklearn.model_selection import train_test_split
+import pickle
+from sklearn.model_selection import KFold
+
+# Import custom modules
+from network import *
+from data import *
+
+dbfile = open('../../Data/full', 'rb')
+data = pickle.load(dbfile)
+dbfile = open('../../Data/location_pert', 'rb')
+location_pert = pickle.load(dbfile)
+dbfile = open('../../Data/pert2profiles', 'rb')
+pert2profiles = pickle.load(dbfile)
+dbfile = open('../../Data/test_perts', 'rb')
+test_pert = pickle.load(dbfile)
+dbfile = open('../../Data/train_perts', 'rb')
+train_pert = pickle.load(dbfile)
+dbfile.close()
+
+# List of all perturbagens
+all_pert = np.concatenate((train_pert, test_pert))
+
+from sklearn.model_selection import KFold
+
+# cross validation code WIP
+all_pert = np.concatenate((train_pert, test_pert))
+
+'''
+ TO-DO: make all variables system arguments: 
+ - epochs
+ - samples pert pert
+ - architecture of siamese (layers and nodes per layer)
+ - name of saved model file
+ '''
+
+
+def cross_validate(splitsize):
+    kf = KFold(n_splits=splitsize)
+    results = []
+    c = 1
+    for train_idx, val_idx in kf.split(all_pert):
+        print("\nCrossvalidation run #", c)
+        c += 1
+        train_perts = all_pert[train_idx]
+        val_perts = all_pert[val_idx]
+        print(train_perts.shape, val_perts.shape)
+        X_train = generate_data(data, train_perts, 100)
+        print("\n")
+        X_val = generate_data(data, val_perts, 100)
+        s = siamese("cos", "net")
+        epochs = 10
+        print("\n")
+        embeddings, trained, pred, p_loss, n_loss, train_acc_l, test_acc_l = run_network(s, epochs, X_train, X_val)
+
+        p = np.sum(trained[0][2] <= 0.5)
+        n = np.sum(trained[0][3] > 0.5)
+        train_acc = ((p + n) / len(X_train[0]) / 2)
+
+        p = np.sum(pred[0][2] <= 0.5)
+        n = np.sum(pred[0][3] > 0.5)
+        val_acc = ((p + n) / len(X_val[0]) / 2)
+
+        accuracy = (train_acc, val_acc)
+        results.append(accuracy)
+    return results
+
+
+result = cross_validate(5)
+test_res = 0
+train_res = 0
+for i in result:
+    test_res += i[1]
+    train_res += i[0]
+print("\n\nCross-validation result: Train: \n%s\t\t Test:%s" % (train_res, test_res))
