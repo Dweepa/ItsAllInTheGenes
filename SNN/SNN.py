71e306cabde6d3c7d6596f0a53655b0b01830124
Dweepa <dweepa.prasad@gmail.com>
2019-10-16 14:48:01 +0530

    Fixed same embedding problem by reverting to OG solution

--- SNN/SNN.py
+++ SNN/SNN.py
@@ -54,11 +54,11 @@ for a in range(len(y)):
         num+=1
     y[a] = pert_dict[y[a]]
 
-X = X[:100]
-y = np.asarray(y[:100]).flatten()
+# X = X[:100]
+# y = np.asarray(y[:100]).flatten()
 # y_small
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 input_size = 978
 # n_layers = 16
