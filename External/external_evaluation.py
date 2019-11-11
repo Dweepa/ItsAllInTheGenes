a67215178bb35f5b76f405dd65a1c42f999d6161
KavyaVarma <kavya.varma98@gmail.com>
2019-11-11 11:43:31 +0530

    Changed External evaluation

--- External/external_evaluation.py
+++ External/external_evaluation.py
@@ -5,16 +5,34 @@ from sklearn.preprocessing import normalize
 import random
 from sklearn.metrics import roc_curve, roc_auc_score, auc
 import sklearn.metrics as metrics
+import sys
+from collections import Counter
 
-embedding_length = 8
+embedding_name = sys.argv[1]
 
-data_array = normalize(np.random.rand(10000, embedding_length))
-data = pd.DataFrame(data_array,	columns = ['e'+str(a+1) for a in range(embedding_length)])
+embedding_length = int(embedding_name.split("_")[4].split("-")[0])
 
-data["pert_id"] = [chr(random.randint(0, 26)+65) for a in range(10000)]
-data["pert_class"] = [random.randint(0, 10) for a in range(10000)]
+# data_array = normalize(np.random.rand(10000, embedding_length))
+# data = pd.DataFrame(data_array,	columns = ['e'+str(a+1) for a in range(embedding_length)])
 
+# data["pert_id"] = [chr(random.randint(0, 26)+65) for a in range(10000)]
+# data["pert_class"] = [random.randint(0, 10) for a in range(10000)]
+
+data = pd.read_csv(embedding_name)
+
+external_data = pd.read_csv("./Final/all3_without_nan.csv")
+perturbagens = [key for key, value in dict(Counter(external_data.id)).items() if value==1]
+
+data = data[data.pert_id.isin(perturbagens)]
+
+pert_class = {}
+for ind in external_data.index:
+	pert_class[external_data.id[ind]] = external_data.atc[ind][:1]
+
+data["pert_class"] = [pert_class[data.pert_id[ind]] for ind in data.index]
+print(data["pert_class"])
 # print(data.head())
+# print(len(data))
 
 def softmax_function(x):
 	e_x = np.exp(x - np.max(x))
@@ -35,6 +53,8 @@ def external_evaluate(data, embedding_length, sample_size, number_test_cases):
 		all_outputs[key] = []
 
 	for a in range(number_test_cases):
+		output = [np.mean(all_outputs[key]) for key in all_keys]
+		sys.stdout.write("\r%d/%d AUC: %f Top-0.1: %f" % (a, number_test_cases, output[-1], output[-5]))
 		data_small = data.sample(sample_size)
 
 		X = np.asarray(data_small.loc[:, 'e1':'e' + str(embedding_length)])
@@ -85,8 +105,9 @@ def external_evaluate(data, embedding_length, sample_size, number_test_cases):
 			all_outputs[key].append(imp_q_val[key])
 
 
+	sys.stdout.write("\r%d/%d\nCompleted\n" % (number_test_cases, number_test_cases))
 	output = [np.mean(all_outputs[key]) for key in all_keys]
 	return output
 
-print(external_evaluate(data, embedding_length, 100, 10))
+print(external_evaluate(data, embedding_length, 10000, 500))
 
